a91265faf4a50b416a6a78c14d64c676be88c121
TinaTinaJ <jishkarianitiko@gmail.com>
2025-01-18 14:00:53 +0400

    Initial Xcode project setup

---
+++ BudgetTrackerApp/BudgetTrackerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BudgetTrackerApp
+//
+//  Created by Tiko on 18.01.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
